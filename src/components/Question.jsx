41ac1a5e592c258e43fe5366108d48658c1eda48
Ivna <ivnacontato.dev@gmail.com>
2023-11-29 21:45:12 -0300

    adding question style css

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
+import "./Question.css";
 
 const Question = () =>{
     const [quizState, dispatch] = useContext(QuizContext);
