4d00a1bebfdefeba5e70a29c9457c84096dd356c
Ivna <ivnacontato.dev@gmail.com>
2023-12-02 17:48:23 -0300

    adding GameOver style

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,19 @@
 import React from "react"
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import './GameOver.css';
+
+import WellDone from '../assets/img/welldone.svg'
 
 const GameOver = () =>{
     return(
-        <div>
-            <h2>Game Over</h2>
+        <div id="gameover"> 
+            <h2>Fim de jogo!</h2>
+            <p>Pontuação: x</p>
+            <p>Você acertou y de z perguntas.</p>
+            <img src={WellDone} alt="Fim de jogo" />
+            <button>Reiniciar</button>
         </div>
     )
 }
